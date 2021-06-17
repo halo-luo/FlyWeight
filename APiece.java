7840f27c46458230a8277cbcdbe124b74c95f621
白哥 <80096029+halo-luo@users.noreply.github.com>
2021-06-17 21:15:22 +0800

    Add files via upload

---
+++ APiece.java
@@ -0,0 +1,10 @@
+package flyWeight;
+
+public abstract class APiece {
+    protected String inKind;
+    public APiece(String inKind){
+        this.inKind = inKind;
+    }
+    //下棋时落子的位置
+    public abstract void Play(int x,int y);
+}
---
+++ Client.java
@@ -0,0 +1,26 @@
+package flyWeight;
+
+import prototype.Apple;
+
+import java.sql.SQLOutput;
+import java.util.Random;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Random rm = new Random();
+        PieceFactory pFactory = new PieceFactory();
+        for(int i = 0;i < 19;i ++)
+            for(int j = 0;j < 19;j ++){
+                APiece p;
+                if(rm.nextInt()%2 == 0)
+                    p = pFactory.GetPiecce("白棋");
+                else
+                    p = pFactory.GetPiecce("黑棋");
+                p.Play(rm.nextInt(19),rm.nextInt(19));
+            }
+        System.out.println("总共棋子对象的个数是："+ pFactory.GetPieceCount());
+    }
+
+
+}
